a184f780e5432c5328c900773a2f8329219440e7
Sapthagiri .P <sapthagiri2110@gmail.com>
2025-09-03 05:58:54 +0530

    Switched to HashRouter for github pages compatibility.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomerListPage from './pages/CustomerListPage';
 import CustomerDetailPage from './pages/CustomerDetailPage';
 import CustomerFormPage from './pages/CustomerFormPage';
@@ -7,12 +7,14 @@ import './App.css';
 
 const App =()=> {
   return (
-    <Routes>
-      <Route path="/" element={<CustomerListPage />} />
-      <Route path="/customers/:id" element={<CustomerDetailPage />} />
-      <Route path="/customers/new" element={<CustomerFormPage />} />
-      <Route path="/customers/:id/edit" element={<CustomerFormPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CustomerListPage />} />
+        <Route path="/customers/:id" element={<CustomerDetailPage />} />
+        <Route path="/customers/new" element={<CustomerFormPage />} />
+        <Route path="/customers/:id/edit" element={<CustomerFormPage />} />
+      </Routes>
+    </Router>
   );
 }
 
